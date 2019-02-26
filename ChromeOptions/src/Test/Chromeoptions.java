5fce0b49ce39a8b57393e0ef4c787c83b58720c2
arunrajrat <37080989+arunrajrat@users.noreply.github.com>
2019-02-26 14:00:41 +0530

    This is ChromeOptions Project files

---
+++ ChromeOptions/src/Test/Chromeoptions.java
@@ -0,0 +1,12 @@
+package Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class Chromeoptions {
+	public static ChromeOptions Chrome_Options(){
+		ChromeOptions option=new ChromeOptions();
+		option.addArguments("--start-maximized","--ignore-certificate-errors","--disable-popup-blocking","--incognito","--disable-infobar");
+		System.out.println("Chrome Desired cap");
+		return option;
+	}
+}
